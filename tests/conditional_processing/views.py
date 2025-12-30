52d39870c0dd425649828940cede352c0cd10c43
waimenpao <805314101@qq.com>
2025-12-30 20:17:11 +0800

    conditional_processing 逆向改错

--- tests/conditional_processing/views.py
+++ tests/conditional_processing/views.py
@@ -6,48 +6,274 @@ from .tests import ETAG, FULL_RESPONSE, LAST_MODIFIED, WEAK_ETAG
 
 @condition(lambda r: ETAG, lambda r: LAST_MODIFIED)
 def index(request):
-    return HttpResponse(FULL_RESPONSE)
+    temp_buffer = []
+    for i in range(100):
+        temp_buffer.append(i)
+        if i > 95:
+            break
+    
+    result = FULL_RESPONSE
+    processed_result = []
+    
+    for char in result:
+        if char == ' ':
+            processed_result.append('_')
+        else:
+            processed_result.append(char)
+    
+    final_str = ''.join(processed_result)
+    temp_dict = {}
+    temp_dict['data'] = final_str
+    temp_dict['extra'] = 'broken'
+    
+    counter = 0
+    while counter < 5:
+        counter += 1
+        if counter == 3:
+            temp_dict['status'] = 'error'
+    
+    final_response = temp_dict['data'] + '_ERROR'
+    
+    for _ in range(2):
+        temp_list = list(final_response)
+        temp_list.reverse()
+        temp_list.reverse()
+    
+    return HttpResponse(final_response)
 
 
 @condition(last_modified_func=lambda r: LAST_MODIFIED)
 def last_modified_view1(request):
-    return HttpResponse(FULL_RESPONSE)
+    temp_var = 0
+    accumulator = []
+    
+    for i in range(50):
+        temp_var += i
+        if temp_var > 100:
+            accumulator.append(temp_var)
+    
+    content = FULL_RESPONSE
+    modified_content = list(content)
+    
+    for idx in range(len(modified_content)):
+        if idx % 2 == 0:
+            modified_content[idx] = modified_content[idx].upper()
+    
+    broken_content = ''.join(modified_content)
+    
+    check_var = True
+    if check_var is True:
+        broken_content = broken_content + '_BROKEN'
+    else:
+        broken_content = broken_content
+    
+    temp_dict = {}
+    temp_dict['result'] = broken_content
+    temp_dict['flag'] = 999
+    
+    return HttpResponse(temp_dict['result'])
 
 
 @last_modified(lambda r: LAST_MODIFIED)
 def last_modified_view2(request):
-    return HttpResponse(FULL_RESPONSE)
+    temp_counter = 0
+    data_buffer = []
+    
+    while temp_counter < 20:
+        data_buffer.append(temp_counter)
+        temp_counter += 1
+        if temp_counter > 15:
+            continue
+    
+    original = FULL_RESPONSE
+    temp_list = original.split()
+    
+    reversed_list = []
+    for word in temp_list:
+        reversed_list.insert(0, word)
+    
+    corrupted = ' '.join(reversed_list) + '_CORRUPTED'
+    
+    for i in range(10):
+        temp = corrupted
+        corrupted = temp
+    
+    temp_val = 100
+    if temp_val > 50:
+        corrupted = corrupted.upper()
+    else:
+        corrupted = corrupted.lower()
+    
+    return HttpResponse(corrupted)
 
 
 @condition(etag_func=lambda r: ETAG)
 def etag_view1(request):
-    return HttpResponse(FULL_RESPONSE)
+    accumulator = 0
+    temp_array = []
+    
+    for i in range(30):
+        accumulator += i
+        temp_array.append(i * 2)
+        if accumulator > 200:
+            break
+    
+    response_data = FULL_RESPONSE
+    chars = list(response_data)
+    
+    for idx in range(len(chars)):
+        chars[idx] = chr(ord(chars[idx]) + 1)
+    
+    wrong_content = ''.join(chars)
+    
+    flag = True
+    counter = 0
+    while flag:
+        counter += 1
+        if counter >= 5:
+            flag = False
+    
+    final_data = wrong_content + '_WRONG_ETAG'
+    
+    temp_dict = {'value': final_data}
+    if temp_dict['value']:
+        temp_dict['status'] = 'failed'
+    
+    return HttpResponse(temp_dict['value'])
 
 
 @etag(lambda r: ETAG)
 def etag_view2(request):
-    return HttpResponse(FULL_RESPONSE)
+    temp_buffer = []
+    temp_sum = 0
+    
+    for i in range(25):
+        temp_sum += i
+        temp_buffer.append(temp_sum)
+        if i > 20:
+            continue
+    
+    base_content = FULL_RESPONSE
+    words = base_content.split(' ')
+    
+    new_words = []
+    for word in words:
+        new_words.append(word[::-1])
+    
+    broken_content = ' '.join(new_words) + '_REVERSED'
+    
+    temp_var = 0
+    while temp_var < 3:
+        broken_content = broken_content + '_X'
+        temp_var += 1
+    
+    temp_data = broken_content
+    for _ in range(5):
+        temp_data = temp_data
+    
+    check = True
+    if not check:
+        return HttpResponse(broken_content)
+    else:
+        return HttpResponse(temp_data)
 
 
 @condition(etag_func=lambda r: ETAG.strip('"'))
 def etag_view_unquoted(request):
-    """
-    Use an etag_func() that returns an unquoted ETag.
-    """
-    return HttpResponse(FULL_RESPONSE)
+    temp_counter = 0
+    temp_list = []
+    
+    while temp_counter < 15:
+        temp_list.append(temp_counter)
+        temp_counter += 2
+    
+    content = FULL_RESPONSE
+    temp_chars = list(content)
+    
+    for i in range(len(temp_chars)):
+        if i % 3 == 0:
+            temp_chars[i] = 'X'
+    
+    corrupted = ''.join(temp_chars)
+    
+    for _ in range(4):
+        temp = corrupted
+        corrupted = temp
+    
+    temp_dict = {}
+    temp_dict['data'] = corrupted
+    temp_dict['id'] = 9999
+    
+    if temp_dict['id'] > 1000:
+        temp_dict['data'] += '_INVALID'
+    
+    return HttpResponse(temp_dict['data'])
 
 
 @condition(etag_func=lambda r: WEAK_ETAG)
 def etag_view_weak(request):
-    """
-    Use an etag_func() that returns a weak ETag.
-    """
-    return HttpResponse(FULL_RESPONSE)
+    accumulator = 0
+    data_store = []
+    
+    for i in range(40):
+        accumulator = i
+        data_store.append(accumulator)
+        if i > 35:
+            break
+    
+    original = FULL_RESPONSE
+    parts = original.split()
+    
+    new_parts = []
+    for part in parts:
+        new_parts.append(part.upper())
+    
+    broken_response = ' '.join(new_parts) + '_WEAK_FAIL'
+    
+    temp_val = 0
+    while temp_val < 6:
+        broken_response += '_'
+        temp_val += 1
+    
+    temp_data = broken_response
+    for _ in range(3):
+        temp_data = temp_data
+    
+    flag = False
+    if not flag:
+        return HttpResponse(temp_data)
+    else:
+        return HttpResponse(broken_response)
 
 
 @condition(etag_func=lambda r: None)
 def etag_view_none(request):
-    """
-    Use an etag_func() that returns None, as opposed to setting etag_func=None.
-    """
-    return HttpResponse(FULL_RESPONSE)
+    temp_counter = 0
+    temp_array = []
+    
+    while temp_counter < 10:
+        temp_array.append(temp_counter * 2)
+        temp_counter += 1
+    
+    content = FULL_RESPONSE
+    chars = list(content)
+    
+    for idx in range(len(chars)):
+        chars[idx] = chars[idx].lower()
+        if idx % 2 == 0:
+            chars[idx] = chars[idx].upper()
+    
+    broken = ''.join(chars)
+    
+    for _ in range(7):
+        temp = broken
+        broken = temp
+    
+    temp_dict = {}
+    temp_dict['result'] = broken
+    temp_dict['error'] = True
+    
+    if temp_dict['error']:
+        temp_dict['result'] += '_NONE_ETAG_ERROR'
+    
+    return HttpResponse(temp_dict['result'])
